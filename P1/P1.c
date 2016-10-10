678ed7a837db958055ba95c731734301bcfa3466
ANGEL BERGANTIÑOS YESTE <abergaye8.alumnes@ub052229.mat.ub.edu>
2016-10-10 12:02:51 +0200

    ICC-10-10-16_1

--- P1/P1.c
+++ P1/P1.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
-int resultrisup(int, double**, double*, double*, double);
+int resoltrisup(int, double**, double*, double*, double);
 int main(void){
 	int n, i, j;
+	double tol=1.0E-3;	
 	double **A, *b, *x;
 	printf("Doneu la dimensio de la matriu, n = \n");
 	scanf("%d", &n);
@@ -20,6 +21,7 @@ int main(void){
 			for(j = 0; j < n; j++){
                         	A[i][j]=0;
                 	}
+		}
 	}
 	b = (double*)malloc( n*sizeof(double) );
 	x = (double*)malloc( n*sizeof(double) );
@@ -35,12 +37,28 @@ int main(void){
 	}
 	printf("Doneu els %d elements del vector b \n", n);
 	for(i = 0; i < n; i++)scanf("%le", &b[i]);
+	if(resoltrisup(n,A,b,x,tol)==0){
+		printf("( ");
+		for(i=0;i<n;i++)printf("%2.2f ", x[i]);
+		printf(")\n");
+	}
 	return 0;
 }
 
-int resoltrisup (int n, double **A, double *b, double *x, double tol){
+int resoltrisup(int n, double **A, double *b, double *x, double tol){
 	int i, j;
 	for(i=n-1;i>=0;i--){
-	
+		for(j=n-1;j!=i;j--){
+			b[i]-=(A[i][j]*x[j]);
+		}
+		if(abs(A[i][i])<tol && abs(b[i])>=tol){
+			printf("No hi ha solució\n");
+			return 1;
+		}else if(abs(A[i][i])<tol && abs(b[i])<tol){
+			x[i]=0;
+		}else{
+			x[i]=b[i]/A[i][i];
+		}
+	}
 	return 0;
 }
