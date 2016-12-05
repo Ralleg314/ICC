b3785d78b3ee36808945d8e73d6b6bb2dfe953bf
ANGEL BERGANTIÑOS YESTE <abergaye8.alumnes@ub052229.mat.ub.edu>
2016-12-05 11:55:07 +0100

    ICC-05-12-16_1

--- B-Bergantiños-Yeste-Angel-2/interpolar.c
+++ B-Bergantiños-Yeste-Angel-2/interpolar.c
@@ -2,6 +2,8 @@
 #include<stdlib.h>
 #include<math.h>
 
+#define PI 3.141592653589793
+
 void dif_dividides(double*,double*,int);
 double aval(double*,double*,int,double);
 double f(double);
@@ -9,7 +11,7 @@ double f(double);
 
 int main(void){
 	int i, n;
-	double *x, *y;
+	double *x, *y, step;
 	printf("Introdueix la dimensio: ");
 	scanf("%d",&n);
 	x=(double*)malloc((n+1)*sizeof(double));
@@ -17,12 +19,13 @@ int main(void){
 	if(x==NULL || y==NULL){
 		return 1;
 	}
+	step=2*PI/n;
 	for(i=0;i<=n;i++){
-		scanf("%lf", &x[i]);
+		x[i]=0+i*step;
 		y[i]=f(x[i]);
 	}
 	dif_dividides(x,y,n);
-	printf("%e\n", aval(x,y,n,x[0]));
+	printf("Pol: %e\nf(t): %e\n", aval(x,y,n,x[0]), f(x[0]));
 	return 0;
 }
 
@@ -32,18 +35,16 @@ void dif_dividides(double *x,double *y, int n){
 		for(j=n;j>=i;j--){
 			y[j]=(y[j]-y[j-1])/(x[j]-x[j-i]);
 		}
-		printf("%e", y[i]);
 	}
-	printf("\n");
 }
 
 double aval(double *x, double *c, int n, double t){
 	int i,j;
-	double sol,r;
-	for(i=n;i>=0;i--){
+	double sol=0,r;
+	for(i=0;i<n+1;i++){
 		r=1;
 		for(j=0;j<i;j++){
-			r*=(t-x[i]);
+			r*=(t-x[j]);
 		}
 		sol+=c[i]*r;
 	}
